e3f27ebf095908c1a92d17a0a5eb60c643e862bd
unknown <madhavkasare@gmail.com>
2023-12-03 23:04:09 +0530

    data ingestion notebook added

--- README.md
+++ README.md
@@ -1,1 +1,12 @@
-# Text-Summarizer
+# End to end text-summarizer project
+
+## Workflows
+
+1. Update config.yamla 
+2. update params.yaml 
+3. update entity
+4. update the configuration manager in src config
+5. update the components
+6. updat the pipeline
+7. update the main.py
+8. update the app.py
---
+++ src/textSummarizer/utils/common.py
@@ -0,0 +1,58 @@
+import os
+from box.exceptions import BoxValueError
+import yaml
+from textSummarizer.logging import logger
+from ensure import ensure_annotations
+from box import ConfigBox
+from pathlib import Path
+from typing import Any
+
+@ensure_annotations
+def read_yaml(path_to_yaml:Path) -> ConfigBox:
+    """ reads yaml file and returns
+    args: path_to_yaml(str): path like input 
+    
+    raises:
+        ValueError if yaml file is empty
+        e: empty file
+    
+    returns: 
+        ConfigBox: config object
+    """
+
+    try:
+        with open(path_to_yaml) as yaml_file:
+            content=yaml.safe_load(yaml_file)
+            logger.info(f"yaml file: {path_to_yaml} loaded successfully")
+            return ConfigBox(content)
+    except BoxValueError:
+        raise ValueError("yaml file is empty")
+    except Exception as e:
+        raise e
+    
+
+@ensure_annotations
+def create_directories(path_to_directories:list, verbose=True): 
+    """ Create list of directories
+    
+    args: 
+        path_to_directories (list): list of path of directories
+        ignore_log (bool, optional): ignore if multiple dirs is to be created. Default is False
+    """
+    for path in path_to_directories:
+        os.makedirs(path,exist_ok=True)
+        if verbose:
+            logger.info(f"Created directory at : {path}")
+
+
+@ensure_annotations
+def get_size(path:Path) -> str:
+    """get size of file in KB
+    
+    args: 
+        path (Path): path to file
+    returns: 
+        str: size in KB    
+    """
+    size_in_kb=round(os.path.getsize(path)/1024)
+    return f"~{size_in_kb} KB"
