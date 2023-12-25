955df7497a257f068f30cd839d92a318134528c3
unknown <madhavkasare@gmail.com>
2023-12-25 13:12:12 +0530

    prediction & UI added

--- src/textSummarizer/config/configuration.py
+++ src/textSummarizer/config/configuration.py
@@ -75,15 +75,14 @@ class ConfigurationManager:
         return model_trainer_config
     
     class ConfigurationManager:
-    def __init__(
-        self,
-        config_filepath = CONFIG_FILE_PATH,
-        params_filepath = PARAMS_FILE_PATH):
-        
-        self.config= read_yaml(config_filepath)
-        self.params= read_yaml(params_filepath)
+        def __init__(
+                self,
+                config_filepath = CONFIG_FILE_PATH,
+                params_filepath = PARAMS_FILE_PATH):
+            self.config= read_yaml(config_filepath)
+            self.params= read_yaml(params_filepath)
 
-        create_directories([self.config.artifacts_root])
+            create_directories([self.config.artifacts_root])
 
     def get_model_evaluation_config(self) -> ModelEvaluationConfig:
         config= self.config.model_evaluation
