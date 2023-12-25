78db845ecce89252fd682a7717791c5eebd9ef9c
unknown <madhavkasare@gmail.com>
2023-12-25 11:58:46 +0530

    Model evaluation completed

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
 from textSummarizer.pipeline.stage_01_data_ingestion import DataIngestionTrainingPipeline
 from textSummarizer.pipeline.stage_02_data_validation import DataValidationPipeline
+from textSummarizer.pipeline.stage_03_data_transformation import DataTransformationPipeline
+from textSummarizer.pipeline.stage_04_model_trainer import ModelTrainingPipeline
+from textSummarizer.pipeline.stage_05_model_evaluation import ModelEvaluationPipeline
 from textSummarizer.logging import logger
 
 
@@ -20,6 +23,38 @@ try:
     logger.info(f">>>>> stage {STAGE_NAME} started <<<<<<")
     data_validation = DataValidationPipeline()
     data_validation.main()
+    logger.info(f">>>>> stage {STAGE_NAME} completed <<<<<<\n\nx==============x")
+except Exception as e:
+    logger.exception(e)
+    raise e
+
+
+STAGE_NAME= "Data Tranformation Stage"
+try:
+    logger.info(f">>>>> stage {STAGE_NAME} started <<<<<<")
+    data_transformation = DataTransformationPipeline()
+    data_transformation.main()
+    logger.info(f">>>>> stage {STAGE_NAME} completed <<<<<<\n\nx==============x")
+except Exception as e:
+    logger.exception(e)
+    raise e
+
+
+STAGE_NAME= "Model Trainer Stage"
+try:
+    logger.info(f">>>>> stage {STAGE_NAME} started <<<<<<")
+    model_trainer = ModelTrainingPipeline()
+    model_trainer.main()
+    logger.info(f">>>>> stage {STAGE_NAME} completed <<<<<<\n\nx==============x")
+except Exception as e:
+    logger.exception(e)
+    raise e
+
+STAGE_NAME= "Model Evaluation Stage"
+try:
+    logger.info(f">>>>> stage {STAGE_NAME} started <<<<<<")
+    model_evaluation = ModelEvaluationPipeline()
+    model_evaluation.main()
     logger.info(f">>>>> stage {STAGE_NAME} completed <<<<<<\n\nx==============x")
 except Exception as e:
     logger.exception(e)
--- src/textSummarizer/config/configuration.py
+++ src/textSummarizer/config/configuration.py
@@ -1,6 +1,6 @@
 from textSummarizer.constants import *
 from textSummarizer.utils.common import read_yaml, create_directories
-from textSummarizer.entity import (DataIngestionConfig, DataValidationConfig)
+from textSummarizer.entity import (DataIngestionConfig, DataValidationConfig,DataTransformationConfig,ModelTrainerConfig,ModelEvaluationConfig)
 
 class ConfigurationManager:
     def __init__(
@@ -38,3 +38,63 @@ class ConfigurationManager:
             ALL_REQUIRED_FILES= config.ALL_REQUIRED_FILES
         )
         return data_validation_config
+    
+
+    def get_data_transformation_config(self) -> DataTransformationConfig:
+        config= self.config.data_transformation
+
+        create_directories([config.root_dir])
+
+        data_transformationtion_config= DataTransformationConfig(
+            root_dir= config.root_dir,
+            data_path= config.data_path,
+            tokenizer_name= config.tokenizer_name
+        )
+        return data_transformationtion_config
+    
+    def get_model_trainer_config(self) -> ModelTrainerConfig:
+        config= self.config.model_trainer
+        params= self.params.TrainingArguments
+
+        create_directories([config.root_dir])
+
+        model_trainer_config= ModelTrainerConfig(
+            root_dir= config.root_dir,
+            data_path= config.data_path,
+            model_ckpt= config.model_ckpt,
+            num_train_epochs= params.num_train_epochs,
+            warmup_steps= params.warmup_steps,
+            per_device_train_batch_size= params.per_device_train_batch_size,
+            weight_decay= params.weight_decay,
+            logging_steps= params.logging_steps,
+            evaluation_strategy= params.evaluation_strategy,
+            eval_steps= params.eval_steps,
+            save_steps= params.save_steps,
+            gradient_accumulation_steps= params.gradient_accumulation_steps
+        )
+        return model_trainer_config
+    
+    class ConfigurationManager:
+    def __init__(
+        self,
+        config_filepath = CONFIG_FILE_PATH,
+        params_filepath = PARAMS_FILE_PATH):
+        
+        self.config= read_yaml(config_filepath)
+        self.params= read_yaml(params_filepath)
+
+        create_directories([self.config.artifacts_root])
+
+    def get_model_evaluation_config(self) -> ModelEvaluationConfig:
+        config= self.config.model_evaluation
+
+        create_directories([config.root_dir])
+
+        model_evaluation_config= ModelEvaluationConfig(
+            root_dir= config.root_dir,
+            data_path= config.data_path,
+            model_path= config.model_path,
+            tokenizer_path= config.tokenizer_path,
+            metric_file_name= config.metric_file_name
+        )
+        return model_evaluation_config
